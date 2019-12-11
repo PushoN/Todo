343dabf93494d4aa6964b4cfdd7626d075995b8e
Alexey <prog.uspn@gmail.com>
2019-12-11 22:14:58 +0700

    lesson 22
    Added fillTasks() by RxJS, displaying tasks by Category in html.

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import {Category} from '../model/Category';
 import {TestData} from '../data/TestData';
 import {Task} from '../model/Task';
+import {Subject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
+
+  taskSubject = new Subject<Task[]>();
 
   constructor() { }
 
@@ -15,14 +18,13 @@ export class DataHandlerService {
     return TestData.catigories;
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasks() {
+    this.taskSubject.next(TestData.tasks);
   }
 
-  getTasksByCategory(category: Category): Task[] {
-    const tasks = this.getTasks().filter(task => task.category === category);
-    console.log(tasks);
-    return tasks;
+  fillTasksByCategory(category: Category) {
+    const tasks = TestData.tasks.filter(task => task.category === category);
+    this.taskSubject.next(tasks);
   }
 
 
